71d51d7835427d2776cfc2e1806945a398dd63ad
Aluno ProWay <aluno@proway.treina>
2026-08-11 21:35:28 -0300

    feat: diretivas angular

--- angular/fundamentos/src/app/components/card-aluno/card-aluno.component.ts
+++ angular/fundamentos/src/app/components/card-aluno/card-aluno.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input, input } from '@angular/core';
 import { Aluno } from '../../interfaces/aluno';
+import { NgClass } from "../../../../node_modules/@angular/common"; 
 
 @Component({
   selector: 'app-card-aluno',
-  imports: [],
   templateUrl: './card-aluno.component.html',
   styleUrl: './card-aluno.component.css',
+  imports: [NgClass],
 })
 export class CardAlunoComponent {
   @Input({required: true}) aluno!: Aluno 
--- angular/fundamentos/src/app/pages/home/home.component.ts
+++ angular/fundamentos/src/app/pages/home/home.component.ts
@@ -11,22 +11,25 @@ import { Aluno } from '../../interfaces/aluno';
 })
 export class HomeComponent {
   title: string = 'Lista de alunos: ';
-  aluno1: Aluno = {
-    name: 'Heloisa C. da Silva',
-    email: 'heloisa@mail.com',
-    class: 'Adas Dev 8',
-    grade: 8,
-    status: GradeVerify(8),
-    imageLink:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEg6_hCRC5eERNRa0IKABVYgkvXSMGER60dIDeYnTILgRVc0eZUTmslog&s=10',
-  };
-  aluno2: Aluno = {
-    name: 'Leonardo Marchioro',
-    email: 'leonardo@mail.com',
-    class: 'ADS',
-    grade: 9,
-    status: GradeVerify(9),
-    imageLink:
-      'https://comicvine.gamespot.com/a/uploads/square_small/6/66303/4469088-tumblr_inline_n0aleph3fl1r8rr6o.jpg',
-  };
+  modoExibicao: string = 'cards';
+  alunos: Aluno[] = [
+    {
+      name: 'Heloisa C. da Silva',
+      email: 'heloisa@mail.com',
+      class: 'Adas Dev 8',
+      grade: 8,
+      status: GradeVerify(8),
+      imageLink:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEg6_hCRC5eERNRa0IKABVYgkvXSMGER60dIDeYnTILgRVc0eZUTmslog&s=10',
+    },
+    {
+      name: 'Leonardo Marchioro',
+      email: 'leonardo@mail.com',
+      class: 'ADS',
+      grade: 4,
+      status: GradeVerify(4),
+      imageLink:
+        'https://comicvine.gamespot.com/a/uploads/square_small/6/66303/4469088-tumblr_inline_n0aleph3fl1r8rr6o.jpg',
+    }
+  ];
 }
